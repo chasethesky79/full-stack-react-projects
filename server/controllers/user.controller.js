e726d295c0e0d805da6cafe91d6d8746209086e6
chasethesky79 <seshadri.bharath@yahoo.com>
2024-01-28 16:32:16 -0600

    Impleemnted delete and update methods

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -23,12 +23,59 @@ const list = async (_1, res) => {
     }
 }
 
+const userByID = async (req, res, next, id) => {
+    try {
+        const user = await User.findById(id)
+        if (!user) {
+            return res.status(400).json({
+                error: 'User not found'
+            })
+        }
+        req.profile = user
+        next()
+    } catch (err) {
+
+    }
+}
+
+const read = (req, res) => {
+    req.profile.hashed_password = undefined
+    req.profile.salt = undefined
+    return res.json(req.profile)
+}
+
+const update = async (req, res) => {
+    try {
+        let user = req.profile
+        user = extend(user, req.body)
+        user.updated = Date.now()
+        await user.save()
+        user.hashed_password = undefined
+        user.salt = undefined
+        return res.json(user)
+    } catch (err) {
+        return generateErrorResponse(err)
+    }
+}
+
+const remove = async (req, res) => {
+    try {
+        const user = req.profile
+        const deletedUser = await user.remove()
+        deletedUser.hashed_password = undefined
+        deletedUser.salt = undefined
+        return res.json(deletedUser)
+    } catch (err) {
+        return generateErrorResponse(err)
+    }
+}
+
 function generateErrorResponse(res, err) {
     return res.status(400).json({
         error: getErrorMessage(err)
     })
 }
 
-export { create, list }
+export { create, list, userByID, read, update, remove }
 
 
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { create, list } from '../controllers/user.controller'
+import { create, list, read, userByID, update, remove } from '../controllers/user.controller'
 
 const router = express.Router()
 
@@ -7,5 +7,9 @@ router.route('/api/users')
     .get(list)
     .post(create)
 
+router.param('userId', userByID)
+router.route('/api/users/:userId').get(read)
+router.route('/api/users/:userId').put(update)
+router.route('/api/users/:userId').delete(remove)
 
 export default router
